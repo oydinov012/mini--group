d240b54d50f66c8fd3a04afb342afe682f5f47a5
Ilyos <223318977+Ilyos-akang@users.noreply.github.com>
2026-06-10 12:45:19 +0500

    loyiha boshlandi2

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phobe_number=models.CharField(max_length=30,blank=True,null=True)
-
+    familiya = models.CharField(max_length=200)
 
     def __str__(self):
         return self.get_full_name
