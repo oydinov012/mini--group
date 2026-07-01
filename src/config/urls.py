0dabdf74696fa7b849811acdafba6e21111f9cea
Ilyos <223318977+Ilyos-akang@users.noreply.github.com>
2026-07-01 16:17:53 +0500

    documentatsiya uchun swagger qoshildi

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,3 +1,9 @@
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from django.contrib import admin
 from django.urls import path, include
 from .views import home_page
@@ -7,4 +13,9 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+
+    # documentatsiya uchun
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
